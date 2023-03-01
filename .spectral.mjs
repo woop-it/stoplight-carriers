bab40e9253638b4ccc5d1a40d7a72b0f3a2ce231
stoplight@melting-point.fr <stoplight@melting-point.fr>
2023-03-01 12:03:44 +0100

    Correction valeur CREDIT_CARD

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjkyOTM/spectral.js?branch_slug=1.8.0_LMMP-7343&token=74962cd7-8604-437a-b42e-0cb168ca32aa";
+export default { extends: ruleset };
